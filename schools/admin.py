a84a287c18e441a384ea2d404aa63ec6c5f9ab1c
Bruno Bereoff <bbereoff@gmail.com>
2023-10-12 21:45:09 -0300

    added: entities admin

--- schools/admin.py
+++ schools/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from schools.models import School
+
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "maximum_capacity"]
+    list_filter = ["id", "name", "maximum_capacity"]
+    search_fields = ["id", "name", "maximum_capacity"]
+    ordering = ["name"]
+
+
+admin.site.register(School, SchoolAdmin)
