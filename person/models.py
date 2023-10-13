0f0ce080e16d7540496e31430352881b5b8e0134
Bruno Bereoff <bbereoff@gmail.com>
2023-10-13 11:49:24 -0300

    fix: model object repr

--- person/models.py
+++ person/models.py
@@ -8,17 +8,14 @@ class Person(ModelUUIDBased):
     first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
 
-    def _str_(self) -> str:
-        return f'{self.first_name} {self.last_name}'
-
     class Meta:
         ordering = ['first_name']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Student(Person):
     grade = models.CharField(max_length=50, blank=True, null=True)
     school = models.ForeignKey(
         School, on_delete=models.CASCADE, related_name='students_school')
-
-    def _str_(self) -> str:
-        return f'{self.first_name} {self.last_name}'
