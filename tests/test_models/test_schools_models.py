fba40b284e617334a01c65c46b408600d9942925
Bruno Bereoff <bbereoff@gmail.com>
2023-10-13 18:31:38 -0300

    updated: refactoring

--- tests/test_models/test_schools_models.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-@pytest.mark.django_db
-class TestSchoolModels:
-    def test_str_method(self, school_factory):
-        obj = school_factory(name="test_name", maximum_capacity=400)
-
-        assert obj.__str__() == "test_name"
